d00e23cba3764d483d68ad2c6dbde633313d1ed5
Aladdin Sonni <aladdin.sonni@gmail.com>
2026-05-31 15:24:37 +0200

    Fix

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono';
 
-// import authorRoutes from './routes/author';
+import authorRoutes from './routes/author';
 import authRoutes from './routes/auth';
 import apiKeyRoutes from './routes/apiKey';
 import bookRoutes from './routes/book';
@@ -8,7 +8,7 @@ import swaggerRoute from './routes/swagger';
 
 const app = new Hono();
 
-// app.route('/authors', authorRoutes);
+app.route('/authors', authorRoutes);
 app.route('/books', bookRoutes);
 app.route('/auth', authRoutes);
 app.route('/api-keys', apiKeyRoutes);
